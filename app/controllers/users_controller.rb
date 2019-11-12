c2bec6e3768f9f3c043cf5e0d8f3a364b51298ec
shafrazi <shafrazi@live.com>
2019-11-13 00:06:04 +0530

    finish project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    # @user = User.new(user_params)
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
     if @user.save
       redirect_to new_user_path
@@ -14,9 +14,27 @@ class UsersController < ApplicationController
     end
   end
 
-  # private
+  def edit
+    @user = User.find(params[:id])
+  end
 
-  # def user_params
-  #   params.require(:user).permit(:username, :email, :password)
-  # end
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # get "/new", to: "users#new"
   # get "/create", to: "users#create"
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
