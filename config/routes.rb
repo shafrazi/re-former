853151c591a13f21ae47d5618cff8329c57dc368
shafrazi <shafrazi@live.com>
2019-11-12 18:45:48 +0530

    add plain html form to create users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # get "/new", to: "users#new"
+  # get "/create", to: "users#create"
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
